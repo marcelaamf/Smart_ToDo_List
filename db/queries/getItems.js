2b3bf0aee38be6724d294a8a62e17eba0b7b2461
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-16 17:56:33 -0700

    connect database

--- db/queries/getItems.js
+++ db/queries/getItems.js
@@ -8,16 +8,16 @@ const client = yelp.client('jmr1iku4kt5iynJ92qDPu2rQqleYcq4IyHCxQBBNx2QzHeseSm3K
 
 
 // Use client.search() method to search for businesses based on our params
-const clientSearch = function(){
-client.search({
-  term: 'Four Barrel Coffee',
-  location: 'san francisco, ca',
-}).then(response => {
-  console.log(response.jsonBody.businesses[0].name);
-  return (response.jsonBody.businesses[0].name);
-}).catch(e => {
-  console.log(e);
-});
+const clientSearch = function () {
+  client.search({
+    term: 'Four Barrel Coffee',
+    location: 'san francisco, ca',
+  }).then(response => {
+    console.log(response.jsonBody.businesses[0].name);
+    return (response.jsonBody.businesses[0].name);
+  }).catch(e => {
+    console.log(e);
+  });
 };
 client.eventLookup("oakland-saucy-oakland-restaurant-pop-up").then(response => {
   console.log(response.jsonBody.description);
